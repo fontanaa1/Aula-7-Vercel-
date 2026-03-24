51a7720cd8263bac4327e150b005f379d228155c
Lorena Fontana <lorena.f.souza6@aluno.senai.br>
2026-03-24 08:29:37 -0300

    Add files via upload

--- data/supabase.js
+++ data/supabase.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@supabase/supase-js');
+const { createClient } = require('@supabase/supabase-js')
 require ('dotenv').config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
